663e1345f47b6788356b02f591d345f3da43696a
Moises <20223tn055@utez.edu.mx>
2025-05-27 15:59:44 -0600

    [FIRT COMMIT]

---
+++ src/main/java/utez/edu/mx/cvmsgr9b/Cvmsgr9bApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvmsgr9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvmsgr9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvmsgr9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvmsgr9b/Cvmsgr9bApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvmsgr9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvmsgr9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
